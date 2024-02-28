5e296bcd4adccbbae42c47984dceb36421b8de4b
jambrizz <ambrizjovani@gmail.com>
2024-02-27 20:32:59 -0800

    Hello world program completed

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("My name is Jovani Ambriz");
+﻿using System;
+using System.Threading.Tasks;
+
+// This console application is a simple "Hello, World!" program to test that the .NET Core SDK is installed and working correctly.
+// The program will display a series of messages to the console, with a delay between each message.
+Console.WriteLine("Hello, World!\n");
+
+Thread.Sleep(3000);
+Console.WriteLine("My name is Jovani Ambriz\n");
+
+Thread.Sleep(3000);
+Console.WriteLine("I am a student at BYUI\n");
+
+Thread.Sleep(3000);
+Console.WriteLine("I am studying software development\n");
+
+Thread.Sleep(1000);
+Console.WriteLine("Goodbye!\n");
--- HelloWorld/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74979aba229786b9265889f9cfbecd62645d5e84")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
