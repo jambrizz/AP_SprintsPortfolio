8ba115d6c51df721baba5fada37e4d73e7d3f7d0
jambrizz <ambrizjovani@gmail.com>
2024-03-17 01:34:49 -0700

    Created Add Class need to work on the Add.xaml.cs file

--- Sprint2/Travel/Travel/Travel/obj/Release/netstandard2.0/Travel.AssemblyInfo.cs
+++ Sprint2/Travel/Travel/Travel/obj/Release/netstandard2.0/Travel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Travel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bdc87fa77ee5b0b6ce1443eb855e55c7c0e10ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd02ae1114fda1655af0efac54fc9eb85b467281")]
 [assembly: System.Reflection.AssemblyProductAttribute("Travel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Travel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
