31eb2c1564648d8a04c69d83121c07dcb32810fa
jambrizz <ambrizjovani@gmail.com>
2024-04-02 02:25:07 -0700

    Creation of calculator app for module 3

---
+++ Calculator/src/main/kotlin/Calculator.kt
@@ -0,0 +1,9 @@
+class Calculator {
+    fun main(args: Array<String>) {
+        println("Hello World! this is the calculator class")
+
+        // Try adding program arguments via Run/Debug configuration.
+        // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+        println("Program arguments: ${args.joinToString()}")
+    }
+}
