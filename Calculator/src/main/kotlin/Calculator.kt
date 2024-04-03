54cc9cb56271e3640e3bbdd9204fa92aeb1f72ed
jambrizz <ambrizjovani@gmail.com>
2024-04-03 00:54:17 -0700

    Added the welcome print statement, created while loop to run program until user ends programs. TODO create function to seperate the input for data validation.

--- Calculator/src/main/kotlin/Calculator.kt
+++ Calculator/src/main/kotlin/Calculator.kt
@@ -1,9 +1,38 @@
 class Calculator {
-    fun main(args: Array<String>) {
-        println("Hello World! this is the calculator class")
 
-        // Try adding program arguments via Run/Debug configuration.
-        // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-        println("Program arguments: ${args.joinToString()}")
+    fun main(args: Array<String>) {
+        println("******** Calculator ********")
+        println("This Calculator can perform the following operations:")
+        println("Addition, Subtraction, Multiplication, Division")
+        println("Enter the operation you want to perform: example 1 + 2")
+        println("To exit the program type 'exit'")
+        print("> ")
+        // Read user input using a
+        var input = readLine()
+        var runProgram = true
+        while (runProgram == true) {
+
+
+            println("You entered: $input")
+            print("> ")
+            input = readLine()
+
+            if (input == "exit") {
+                runProgram = false
+                println("Exiting Calculator, Thank you for using it!")
+            }
+            else {
+                //TODO: Call the function to seperateInput to get the numbers and the operator
+            }
+        }
+    }
+
+    fun seperateInput(input: String): List<String> {
+        val stringEquation = input
+
+
+
+        return input.split(" ")
     }
 }
+
